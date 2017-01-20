12c95221122d0da53b638135480259d22cb67dd0
Christopher Gronsseth <cgronseth@gmail.com>
2017-01-20 22:06:14 +0100

    Subida inicial

---
+++ DemoSHP2016/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="fee4734f-259a-4485-af6d-2a37776de7f9" description="Demostración Add-in Sharepoint-hosted" featureId="fee4734f-259a-4485-af6d-2a37776de7f9" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="DemoSHP2016" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="dbf5270a-7149-4edc-a2c0-23bfc6ba9018" />
+    <projectItemReference itemId="c5635e48-e9b6-4774-b3f3-640f09979a84" />
+    <projectItemReference itemId="a5c9e7d8-3eb6-40dd-bbd1-f789caab4d67" />
+    <projectItemReference itemId="44e78894-3a34-4b50-a803-b60ebab91fdb" />
+  </projectItems>
+</feature>
---
+++ DemoSHP2016/Scripts/App.js
@@ -0,0 +1,71 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage() {
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    var urlLista = "https://redcomcibernetico.sharepoint.com/sites/desarrolloapps/AusenciasAPP";
+    var nombreLista = "2017_Ausencias";
+
+    var addinweburl;
+
+    // Main
+    $(document).ready(function () {
+        getUserName();
+        getVacaciones();
+    });
+
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(function () {
+            $('#userName').text('Hola ' + user.get_title());
+        },
+        function (sender, args) {
+            alert('Error:' + args.get_message());
+        });
+    }
+
+    var hostweburl;
+    var addinweburl;
+
+    function getVacaciones() {
+        hostweburl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+        addinweburl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+
+        document.getElementById("web3").innerHTML = "Url lista: " + urlLista;
+        document.getElementById("web1").innerHTML = "Url Host: " + hostweburl;
+        document.getElementById("web2").innerHTML = "Url Addin: " + addinweburl;
+
+        var scriptbase = hostweburl + "/_layouts/15/";
+        $.getScript(scriptbase + "SP.RequestExecutor.js", getVacacionesPaso2);
+    }
+
+    function getVacacionesPaso2() {
+        var executor = new SP.RequestExecutor(addinweburl);
+        executor.executeAsync(
+            {
+                url: addinweburl + "/_api/SP.AppContextSite(@target)/web/lists/getbytitle('" + nombreLista + "')/items?@target='" + urlLista + "'",
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: function (data) {
+                    document.getElementById("lista").innerHTML = "OK!";
+                },
+                error: function (data, errorCode, errorMessage) {
+                    document.getElementById("error").innerHTML = "Error code: " + errorCode + "<br/>" + errorMessage + " :" + data.body;
+                }
+            }
+        );
+    }
+
+    function getQueryStringParameter(paramToRetrieve) {
+        var params = document.URL.split("?")[1].split("&");
+        var strParams = "";
+        for (var i = 0; i < params.length; i = i + 1) {
+            var singleParam = params[i].split("=");
+            if (singleParam[0] == paramToRetrieve)
+                return singleParam[1];
+        }
+    }
+}
